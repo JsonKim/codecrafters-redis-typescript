ec83e6f2c770697544d3240417996492751d1056
Jason Kim <actcrazy@gmail.com>
2024-07-22 20:21:17 +0900

    fix: PING, ECHO, SET, GET을 parser로 구현

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,8 @@
 import * as net from "net";
+import { DataType, parse } from "./parser";
+import { O } from "@mobily/ts-belt";
+
+const database = new Map<string, string>();
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
@@ -7,14 +11,44 @@ console.log("Logs from your program will appear here!");
 const server: net.Server = net.createServer((connection: net.Socket) => {
   // Handle connection
   connection.on("data", (data: Buffer) => {
-    const command = data.toString().trim().split("\r\n");
-    if (command.length === 3 && command[2] === "PING") {
-      connection.write("+PONG\r\n");
+    const command = parse(data);
+
+    if (O.isNone(command)) {
       return;
     }
-    if (command.length === 5 && command[2] === "ECHO") {
-      connection.write(`+${command[4]}\r\n`);
-      return;
+
+    if (command.type === DataType.Array && command.value.length === 1) {
+      if ((command.value[0].value = "PONG")) {
+        connection.write("+PONG\r\n");
+        return;
+      }
+    }
+
+    if (command.type === DataType.Array && command.value.length === 2) {
+      if (command.value[0].value === "ECHO") {
+        connection.write(`+${command.value[1].value}\r\n`);
+        return;
+      }
+    }
+
+    if (command.type === DataType.Array && command.value.length === 3) {
+      if (command.value[0].value === "SET") {
+        const [_, key, value] = command.value;
+        database.set(key.value as string, value.value as string);
+        connection.write("+OK\r\n");
+        return;
+      }
+    }
+
+    if (command.type === DataType.Array && command.value.length === 2) {
+      if (command.value[0].value === "GET") {
+        const key = command.value[1].value as string;
+        const value = database.get(key);
+        if (value) {
+          connection.write(`$${value.length}\r\n${value}\r\n`);
+        }
+        return;
+      }
     }
   });
 });
